263c474a75024fbf89f76bae79e74b97e65b578f
eush77 <eush77@gmail.com>
2013-08-22 03:18:30 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Highlight Links to Wikipedia
+============================
+Append small Wikipedia logos to links which lead to Wikipedia articles. Logos are adjusted to font size.
+
+![](sample.png)
+
+Links on Wikipedia itself are not involved.
---
+++ highlight-links-to-wikipedia.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        Highlight Links to Wikipedia
+// @version     0.3
+// @description Append small Wikipedia logos to links which lead to Wikipedia articles.
+// @include     *
+// @exclude     *://*wikipedia.org/*
+// ==/UserScript==
+
+!function(SCALE) {
+    var urlMatcher = /https?:\/\/([^.\/]+\.)?wikipedia.org\/wiki\//;
+    var logo = (function() {
+        var template = document.createElement('img');
+        template.src = 'http://wikipedia.org/favicon.ico';
+        template.style.verticalAlign = 'super';
+        template.style.padding = '0';
+        template.style.borderWidth = '0';
+        template.style.margin = '0';
+        return function(fontSize) {
+            var logo = template.cloneNode();
+            logo.width = logo.height = fontSize * SCALE;
+            return logo;
+        };
+    }());
+    [].slice.call(document.querySelectorAll('a[href*="wikipedia.org/wiki/"]')).forEach(function(a) {
+        if (urlMatcher.test(a.href)) {
+            var em = parseInt(window.getComputedStyle(a).fontSize);
+            a.parentNode.insertBefore(logo(em), a.nextSibling);
+        }
+    });
+}(0.75);
